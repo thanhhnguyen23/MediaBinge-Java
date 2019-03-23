4506be4ef329dbbf46f6528992520b50aa286b6f
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-23 17:59:46 -0400

    testing hibernate configurations with MySQL

--- src/main/java/com/revature/MediaBingeDriver.java
+++ src/main/java/com/revature/MediaBingeDriver.java
@@ -1,7 +1,8 @@
 package com.revature;
 
-//import java.security.Provider.Service;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.models.User;
@@ -18,10 +19,17 @@ public class MediaBingeDriver {
 	
 
 	public static void main(String[] args) {
+
+		// test mysql connection
+		/////////////////////////
 		
-		User person = new User("supafly","Tabitha", "Wintward","09876");
-		System.out.println(person);
-		System.out.println(service.add(person));
+		
+
+		
+//		User person = new User("supafly","Tabitha", "Wintward","09876");
+//		System.out.println(person);
+//		System.out.println(service.add(person));
+
 		
 		
 	}
